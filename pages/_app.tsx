85c3fd91be521ea604b237ec57262de9f839243d
Jillian Anderson Slate <54810321+jmaslate@users.noreply.github.com>
2021-07-20 11:28:03 +0200

    add circuit & header styles (#2)
    * add inline header styles
    
    * add circuit-ui + deps and style header and button
    
    * make rainbow linear gradient more precise
    
    * replace signup button with styled button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,26 @@
-import React from "react";
-import Head from "next/head";
-import type { AppProps } from "next/app";
-import "../styles/globals.css";
-import initAuth from "../utils/initAuth";
+import React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from 'emotion-theming';
+import { light } from '@sumup/design-tokens';
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+import initAuth from '../utils/initAuth';
 
 initAuth();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Unicorn</title>
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦄</text></svg>"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+	return (
+		<ThemeProvider theme={light}>
+			<Head>
+				<title>Unicorn</title>
+				<link
+					rel='icon'
+					href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦄</text></svg>'
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
 
 export default MyApp;
